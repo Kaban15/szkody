e6a731b8a1c2e358fcd5b29e178d56222268c55d
Kaban <piotrtokeny@gmail.com>
2026-04-11 18:18:53 +0200

    feat: add Chatwoot widget loader script

---
+++ js/chatwoot.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * Chatwoot SDK loader.
+ * Initializes the Chatwoot chat widget on all pages.
+ *
+ * Replace CHATWOOT_DOMAIN_PLACEHOLDER and CHATWOOT_TOKEN_PLACEHOLDER
+ * with real values after Chatwoot VPS setup.
+ */
+(function initChatwoot() {
+    var BASE_URL = 'https://CHATWOOT_DOMAIN_PLACEHOLDER';
+    var TOKEN = 'CHATWOOT_TOKEN_PLACEHOLDER';
+
+    var d = document;
+    var s = d.createElement('script');
+    s.src = BASE_URL + '/packs/js/sdk.js';
+    s.defer = true;
+    s.async = true;
+    s.onload = function () {
+        window.chatwootSDK.run({
+            websiteToken: TOKEN,
+            baseUrl: BASE_URL,
+            locale: 'pl'
+        });
+    };
+    d.head.appendChild(s);
+})();
